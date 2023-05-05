c3096f9b973409f52f75ca91dac46da4753e9639
Anna Manole <am224wd@student.lnu.se>
2023-05-05 15:34:48 +0200

    getServerSideProps instead of static paths

--- pages/tjanster/[id].js
+++ pages/tjanster/[id].js
@@ -2,30 +2,6 @@ import { getTjanster } from '@/sanity/sanity-tjanster'
 import Article from '@/components/Article'
 import ImageFull from '@/components/ImageFull'
 
-export const getStaticPaths = async () => {
-  const tjansterData = await getTjanster()
-  const paths = tjansterData.map((tjanst) => {
-    return {
-      params: { id: tjanst.slug }
-    }
-  })
-
-  return {
-    paths,
-    fallback: false // if a user requests a page that doesn't exist, show 404
-  }
-}
-
-export const getStaticProps = async (context) => {
-  const slug = context.params.id
-  const tjansterData = await getTjanster()
-  const data = tjansterData.filter((tjanst) => tjanst.slug === slug)
-
-  return {
-    props: { data }
-  }
-}
-
 const ServiceDetailPage = ({ data }) => {
   return (
     <div>
@@ -35,6 +11,19 @@ const ServiceDetailPage = ({ data }) => {
       ))}
     </div>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const slug = context.params.id
+  const tjansterData = await getTjanster()
+  const data = tjansterData.filter((tjanst) => tjanst.slug === slug)
+  
+  console.log(data[0].image)
+  console.log(slug)
+  
+  return {
+    props: { data }
+  }
 }
 
 export default ServiceDetailPage
