01053b8d4be75f85d51d2a650463033889cb55a9
Anna Manole <am224wd@student.lnu.se>
2023-05-29 20:54:09 +0200

    try Image component on ImageFull as well

--- components/ImageFull.js
+++ components/ImageFull.js
@@ -1,21 +1,27 @@
+import Image from 'next/image'
+
 /**
  * Full width image component.
  *
  * @param {string} src - Image source.
  * @returns {HTMLElement} ImageFull.
  */
-const ImageFull = ({ src, alt }) => (
-<div className="md:w-5/5 w-full">
-  <div className="relative w-full h-64 md:h-64">
-   {/* eslint-disable-next-line @next/next/no-img-element */}
-    <img
-      src={src}
-      alt={alt}
-      fill="true"
-      className='object-cover object-center w-full h-full'
-    />
-  </div>
-</div>
-)
+const ImageFull = ({ src, alt }) => {
+  console.log(src)
+
+  return (
+    <div className="md:w-5/5 w-full">
+      <div className="relative w-full h-64 md:h-64">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <Image
+          src={src}
+          alt={alt}
+          fill="true"
+          className='object-cover object-center w-full h-full'
+        />
+      </div>
+    </div>
+  )
+}
 
 export default ImageFull
