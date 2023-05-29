68c3c45fae681a893e9c0b328cafddb0b19ff706
Anna Manole <am224wd@student.lnu.se>
2023-05-29 21:00:13 +0200

    Create loader

--- components/ImageFull.js
+++ components/ImageFull.js
@@ -9,12 +9,17 @@ import Image from 'next/image'
 const ImageFull = ({ src, alt }) => {
   console.log(src)
 
+  const imagePreloader = ({ src, width, quality }) => {
+    return `${src}?w=${width}&q=${quality || 75}`
+  }
+
   return (
     <div className="md:w-5/5 w-full">
       <div className="relative w-full h-64 md:h-64">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <Image
           src={src}
+          loader={imagePreloader}
           alt={alt}
           fill="true"
           className='object-cover object-center w-full h-full'
