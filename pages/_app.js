b4531d95dd50c3bba203574f3589474bc2742a1e
Anna Manole <am224wd@student.lnu.se>
2023-10-16 18:23:13 +0200

    meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -17,8 +17,6 @@ export default function App ({ Component, pageProps }) {
       <Head>
       <link rel="icon" href="/images/favicon-filled.png" />
         <title>Allel Elektrokonsult AB</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="Elkonsult, stockholm, projektering, elritningar, elingenjör, förfrågningsunderlag, bygghandlingar,elbesiktning, rambeskrivning"></meta>
       </Head>
       <Layout>
         <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,8 @@ export default function Document () {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;700;900&family=IBM+Plex+Mono&display=swap" rel="stylesheet" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content="Elkonsult, stockholm, projektering, elritningar, elingenjör, förfrågningsunderlag, bygghandlingar,elbesiktning, rambeskrivning"></meta>
       <Head>
       </Head>
       <body>
